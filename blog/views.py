2ed074fb2e3fd7f37e1d73ba4aa13966d79ac3c4
Nicholas <nick.chow.zj@gmail.com>
2016-08-19 17:21:08 +0800

    fixed error

--- blog/views.py
+++ blog/views.py
@@ -49,3 +49,7 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('blog.views.post_list')
+    
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
